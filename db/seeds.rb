2e5e9bec120081558e8ed2d7a95c0b4e8084542e
curlyroots <selimcodeur@gmail.com>
2022-06-14 17:47:27 +0200

    add faker + basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Housing.destroy_all
+User.destroy_all
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+10.times do
+  Housing.create(
+    title: Faker::BossaNova.artist,
+    price: Faker::Number.number(digits: 6),
+    description: Faker::Hacker.say_something_smart,
+    address: Faker::Address.street_address,
+    user: User.all.sample
+  )
+end
